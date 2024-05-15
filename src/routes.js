f76f2180d7a3087dc72e41f6f00df3cd55a5c087
K3yNet <enzo_vel@hotmail.com>
2024-05-15 03:48:32 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 const { Router } = require("express");
 const routes = new Router();
 
-routes.get("/health", (request, response) => {
-    return response.send("Connected with sucsess!");
+routes.get("/health", (req, res) => {
+    return res.send("Connected with sucsess!");
 });
 
 module.exports = routes;
