fe83af2d5ab1e0cc8771cc2b723ac98b418ac5ab
K3yNet <enzo_vel@hotmail.com>
2024-05-16 02:12:50 -0300

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    testEnvironment: 'node',  // Ambiente de teste para aplicações Node.js
+    clearMocks: true,        // Limpa mocks entre os testes
+    coverageDirectory: 'coverage', // Diretório para os relatórios de cobertura
+};
