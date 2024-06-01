01ff636d3067bb02ef85445d6f1e17dd59afc13e
K3yNet <enzo_vel@hotmail.com>
2024-06-01 20:50:22 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ async function startServer() {
     try {
         await connectDB();
         app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port: ${process.env.PORT}`);
+            console.log(`Server is running on port:: ${process.env.PORT}`);
         });
 
     } catch (error) {
